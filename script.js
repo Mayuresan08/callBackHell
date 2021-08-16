3f00d6f4ba95d08c709231330c93793a39b3c8a8
Mayuresan08 <87636548+Mayuresan08@users.noreply.github.com>
2021-08-16 16:58:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+timer=document.getElementById("container")
+
+setTimeout(()=>{
+  timer.innerText="10";
+  setTimeout(()=>{
+      timer.innerText="9";
+      setTimeout(()=>{
+          timer.innerText="8";
+          setTimeout(()=>{
+              timer.innerText="7";
+              setTimeout(()=>{
+                  timer.innerText="6";
+                  setTimeout(()=>{
+                      timer.innerText="5";
+                      setTimeout(()=>{
+                          timer.innerText="4";
+                          setTimeout(()=>{
+                              timer.innerText="3";
+                              setTimeout(()=>{
+                                  timer.innerText="2";
+                                  setTimeout(()=>{
+                                      timer.innerText="1";
+                                      setTimeout(()=>{
+                                          timer.innerText="HAPPY NEW YEAR!!!"
+                                      })
+                                  },1000)
+                              },1000)
+                          },1000)
+                      },1000)
+                  },1000)
+              },1000)
+          },1000)
+      },1000)
+  },1000)
+},1000)
